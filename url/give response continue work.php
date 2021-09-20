94c515417e61dc2e935432ddb3322cfabea019f5
Spsoi <42588004+Spsoi@users.noreply.github.com>
2021-09-20 16:36:58 +0300

    Update give response continue work.php

--- url/give response continue work.php
+++ url/give response continue work.php
@@ -9,3 +9,16 @@
   ob_end_flush();
   ob_flush();
   flush();
+
+OR
+  
+ // header("HTTP/1.0 200 OK");
+    ignore_user_abort(true);
+    set_time_limit(0);
+    ob_start();
+    echo json_encode(['code' => 0]); // если нужно вернуть какие-то данные
+    header('Connection: close');
+    header('Content-Length: '.ob_get_length());
+    ob_end_flush();
+    ob_flush();
+    flush();
